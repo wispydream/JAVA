e16dd8a9cddc3af1fd75e143a9582a4c28902116
wispydream <1079607508@qq.com>
2020-04-28 16:50:30 +0800

    项目结构修改为父子聚合项目

--- spring-cloud-simple-demo/microservice-consumer-feign/src/main/java/com/cloud/feign/FeignApplication.java
+++ spring-cloud-simple-demo/microservice-consumer-feign/src/main/java/com/cloud/feign/FeignApplication.java
@@ -3,13 +3,14 @@ package com.cloud.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan
 @EnableDiscoveryClient
-@EnableFeignClients				// feign方式实现微服务之间通信
+@EnableFeignClients                // feign方式实现微服务之间通信
 
 public class FeignApplication {
 
--- spring-cloud-simple-demo/microservice-consumer-feign/src/main/java/com/cloud/feign/rpc/GetHello.java
+++ spring-cloud-simple-demo/microservice-consumer-feign/src/main/java/com/cloud/feign/rpc/GetHello.java
@@ -1,6 +1,7 @@
 package com.cloud.feign.rpc;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.netflix.feign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
